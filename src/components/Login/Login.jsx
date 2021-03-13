3bdc2b7b80729859f749f046ae0890b6ca079727
Riddhi Siddarkar <riddhisiddarkar@gmail.com>
2021-03-13 16:23:32 +0530

    adjusting code to use new ui elements

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 import styles from "./Login.module.css";
 import loginsvg from "../../assets/svgs/login.svg";
+import Input from "../../UI/Input/Input";
+import Button from "../../UI/Button/Button";
 
 const Login = () => {
   return (
     <div className={styles.login}>
       <form>
-        <input typ="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+        <Input typ="email" placeholder="Email" />
+        <Input type="password" placeholder="Password" />
+        <Button type="submit" title="Login" />
       </form>
       <img src={loginsvg} className={styles.loginimg} alt="login" />
     </div>
